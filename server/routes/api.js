4647186ad42961cbe1e0b1d8eee109e087a1e620
Nick DeRobertis <whoopnip@gmail.com>
2017-03-02 09:03:48 -0500

    Added express server

---
+++ server/routes/api.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+const express = require('express');
+const router = express.Router();
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+module.exports = router;
